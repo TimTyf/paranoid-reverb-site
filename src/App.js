7fb7a98004a6c5027d949c86df1d9d0c7f0faf2b
TimTyf <tim.tyflioris@gmail.com>
2025-02-28 09:11:40 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Paranoid Reverb Official Website</h1>
+    </div>
+  );
+}
+
+export default App;
